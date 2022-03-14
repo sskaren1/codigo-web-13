030d72d94dd8068fa2f043af9cc1ad9f92e598ca
sskaren1 <karen.93st@gmail.com>
2022-03-14 12:26:06 -0500

    semana 7

---
+++ semana7/dia4/prueba.js
@@ -0,0 +1,13 @@
+//  "type": "module", ------- es para poder usar codigo de forma modular,
+// https://www.npmjs.com/package/date-format
+
+// ahora vamos a ver como podemos utilizar la libreria que acabamos de instalar
+// ! La forma antigua en la cual usabamos una libreria es usando require
+// * const format = require("date-format");
+// ! El "nuevo" standart es usando import
+import format from "date-format";
+
+// Luego de importar el format podemos utilizarlo
+const fechaActual = new Date();
+const fechaFormateada = format.asString("dd-MM-yyyy", fechaActual);
+console.log(fechaFormateada);
